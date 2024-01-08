f2136195162f5979aa05834e07b80a543f73fbd1
igorgaidolfi <gandalf.4@outlook.it>
2024-01-08 16:26:42 +0100

    added content to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,45 @@ export default {
 }
 </script>
 <template lang="">
-  <div>
-    
-  </div>
+    <header>
+        <div class="container d-flex">
+            <img src="../assets/img/dc-logo.png" alt="">
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </div>
+    </header>
 </template>
-<style lang="">
+<style lang="scss">
+header {
+    margin: 20px 0;
+}
 
+.d-flex {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+}
+
+li {
+    padding: 0 15px;
+}
+
+img {
+    height: 75px;
+}
 </style>
